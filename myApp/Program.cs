a3a3abd94a41ce6aa1cea2d857b21fea4b7bea5d
Kamil Twarogal <ktw@spyro-soft.com>
2023-04-27 11:45:05 +0200

    3 Fancy methods

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!!!");
+            Console.WriteLine(Add(5, 2));
+            Console.WriteLine(Subtract(5, 2));
+            Console.WriteLine(Multiply(5, 2));
         }
-    }    
+
+        private static int Add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        private static int Subtract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+
+		private static int Multiply(int v1, int v2)
+        {
+            return v1 * v2;
+        }        
+    }
 }
